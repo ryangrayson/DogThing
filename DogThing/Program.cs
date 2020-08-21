97adf680bd01984352c5c6db8430e78d1f40ba06
Ryan Grayson <ryan.g.grayson@gmail.com>
2020-08-21 13:27:54 -0500

    First draft

---
+++ DogThing/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace DogThing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string dogName = "Molly";
+            int dogAge = 5;
+            char firstInitial = 'M';
+            bool genderFemale = true;
+            double pawPads = 16;
+            decimal weight = 73.6m;
+
+            //string interpolation with the $
+
+            Console.WriteLine($"My dog's name is {dogName}, she is {dogAge} and her name, obviously, starts with {firstInitial}. \n" +
+                $"As her name suggests, it is {genderFemale} that she is a lady. She has {pawPads} toes and weighs {weight}");
+
+
+        }
+    }
+}
